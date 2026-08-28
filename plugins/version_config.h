f283467f7d9880d6adc9896d74b50774c3e2e93c
TheBx123 <TheBx123@users.noreply.github.com>
2026-08-28 04:28:47 +0330

    Release v1.2.7: Automated build and release update

--- plugins/version_config.h
+++ plugins/version_config.h
@@ -1,5 +1,5 @@
 #pragma once
 
 // Auto-generated from .env by build.py - DO NOT EDIT DIRECTLY
-#define PLUGIN_VERSION_STR "1.2.6"
+#define PLUGIN_VERSION_STR "1.2.7"
 #define UPDATE_CHECK_URL "https://raw.githubusercontent.com/TheBx123/BlackSpeak/main/version.json"
